9800e065323108ce50a6e781bb6ff46acf471513
katerin <katerinbarrera2001@gmail.com>
2022-02-07 16:15:05 -0500

    favicon añadido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-10 bg-[#c0a0c3] w-full">
-      <div>soy el footeeerr</div>
+    <div className="h-10 bg-[#c0a0c3] w-full flex items-center justify-center space-x-4 text-gray-900 text-lg p-8">
+      <div>
+        <i class="fas fa-phone-alt"></i> +57 3015532120
+      </div>
+      <div>
+        <i class="fas fa-envelope-open-text"></i> katerinbarrera2001@gmail.com
+      </div>
     </div>
   );
 };
